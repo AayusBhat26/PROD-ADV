c6ddb12548031b9b711a2b9cc1a5f085ced97352
AayusBhat26 <100667276+AayusBhat26@users.noreply.github.com>
2023-11-19 00:56:54 +0530

    spinner variants created, able to login

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -6,7 +6,7 @@ const font = Poppins({
       weight:['400', '600']
 });
 export const Logo = () => {
-      return ( <div className="hidden md:flex items-center gap-x-2">
+      return ( <div className="hidden md:flex items-center gap-x-2 ">
             <Image src ="/favicon.ico" alt="LOGO" height={'40'} width={'40'}/>
             <p className={cn("font-semibold", font.className)}>NORD - PRODUCTIVITY AND NOTE TAKING</p>
       </div> );
